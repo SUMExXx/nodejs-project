5b4d0151f622a1e4c3ff7bb1490d76e310a74af1
Suman Debnath <54632628+SUMExXx@users.noreply.github.com>
2022-12-30 06:22:18 +0530

    made some changes to index.html file and started writing the app.js file

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const formData = {}
+
+formElement = document.getElementById("form")
+
+function storeData(event){
+    event.preventDefault();
+    elementsList = document.querySelectorAll(".formStackElements")
+
+    console.log(elementsList)
+    // for(i in elementsList){
+
+    // }
+}
